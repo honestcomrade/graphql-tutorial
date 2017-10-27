a4a61241239da286e781049003e00535975a8114
Joe Saraceno <joe@acutulus.co>
2017-10-26 21:31:26 -0700

    bit of styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ChannelsList from './components/ChannelsList/ChannelsList'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Apollo</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h2>Welcome to Apollo</h2>
+        </div>
+        <ChannelsList/>
       </div>
     );
   }
---
+++ client/src/components/ChannelsList/ChannelsList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './ChannelsList.css';
+
+class ChannelsList extends Component {
+  render() {
+    return (
+      <div>
+        <ul className="Item-list">
+          <li>Channel 1</li>
+          <li>Channel 2</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default ChannelsList;
