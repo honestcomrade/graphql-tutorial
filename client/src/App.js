734bd40632e1c1cba9b5d8b4a46395407c8dff3b
Joe Saraceno <joe@acutulus.co>
2017-10-26 21:33:49 -0700

    fonts

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Apollo</h2>
+          <h2 className="App-intro">Welcome to Apollo</h2>
         </div>
         <ChannelsList/>
       </div>
